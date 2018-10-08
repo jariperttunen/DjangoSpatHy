dad2a0cb49b6dbdd6cad2b5f98b97431a9f69609
jarip <jari.perttunen@luke.fi>
2018-10-08 11:36:35 +0300

    Decided to have DjangoSpaFHy to independent www module. The user will clone required SpaFHy modules from git side by side with this Django www user interface. Seee README.md.

--- Spathy/Spathy/spathypath.py
+++
@@ -1,9 +0,0 @@
-import os
-import pathlib
-setupfile=pathlib.Path('spathy4_default.ini').resolve()
-spathy_path = pathlib.Path('.').resolve()
-SVEcatchmentpath=pathlib.Path('SpathyData','SVEcatchments').resolve()
-SVErunoff_file=pathlib.Path('SpathyData','SVEcatchments','Runoff','Runoffs_SVEcatchments_mmd.csv')
-#print('setupfile:',setupfile)
-#print('spathy_path:',spathy_path)
-#print('SVEcatchmentpath::',SVEcatchmentpath)
--- SpathyDemo/spath.py
+++ SpathyDemo/spath.py
@@ -4,5 +4,5 @@ mainpage='page1_template.html'
 resultpage='page2_template.html'
 maintitle='Spatial Hydrology Demo'
 results='Results'
-spathy_runtime='./spatial_demos.py'
-spathy_dir=os.path.join(BASE_DIR,"Spathy/Spathy/")
+spathy_runtime='./test_spafhy.py'
+spathy_dir=os.path.join(BASE_DIR,"../SpaFHy/")
